43b90d96b8f4c1adb7c3f843bc4315530c93ad09
Somdip Dey <somdip007@gmail.com>
2018-07-14 23:51:25 +0100

    updated summary

--- PSO/interface/_coder_PSO_info.h
+++ PSO/interface/_coder_PSO_info.h
@@ -1,4 +1,5 @@
-/* 
+/*
+ * Copyright @ Somdip Dey, 2018
  * Academic License - for use in teaching, academic research, and meeting 
  * course requirements at degree granting institutions only.  Not for 
  * government, commercial, or other organizational use. 
